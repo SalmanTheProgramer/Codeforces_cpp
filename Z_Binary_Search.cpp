ab8cac77f9ad5e40cd4fb486a9dc310a4d18d5da
SalmanTheProgramer <msk.99401@gmail.com>
2026-06-29 20:43:02 +0600

    Z. Binary Search
    link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Z

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -12,10 +12,41 @@ int main() {
         cin >> v[i];
     }
 
-    // sorting the array
-    sort (v, v + n);
+    // sorting the vector array
+    sort (v.begin(), v.end());
     
-    
+    // execution against test cases
+    while (q--) {
+
+        int x;
+        cin >> x;
+
+        int flag = 0, low = 0, high = n-1;
+
+        while (low <= high) {
+
+            int mid_index = (low + high) / 2;   // calculate mid index
+
+            if (v[mid_index] == x) {    
+
+                flag = 1;
+                break;
+            
+            }else if (v[mid_index] > x) {   
+
+                high = mid_index - 1;
+
+            }else if (v[mid_index] < x) {
+
+                low = mid_index + 1;
+            }
+
+        }
+
+        if (!flag) cout << "not found" << endl;
+        else cout << "found" << endl;
+
+    }
     
     return 0;
 }
